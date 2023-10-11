b5038b856eeb9672d0f0eb3ae8937dd0a88441ae
Mert izgahi <mert.izgahi@outlook.com>
2023-10-11 15:49:03 +0300

    fix images

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,6 @@
-import Head from "next/head";
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Trippian</title>
-      </Head>
       <body>{children}</body>
     </html>
   );
